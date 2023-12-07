cebc8e297b050063670d85e60b038167e7d53dd8
nikhil030901 <jumbo.nikhil19@gmail.com>
2023-12-07 22:39:28 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config();
 import userRouter from './routes/user.route.js'; 
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
+dotenv.config();
 mongoose.connect(process.env.MONGO).then(() =>{
     console.log('Connected to MongoDB');
 })
@@ -15,7 +16,9 @@ mongoose.connect(process.env.MONGO).then(() =>{
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 const port = 3000;
+
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}!!`);
 })
