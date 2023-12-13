4b1e51727a4985dfadef3a41d4e79e0ae50ecb98
nikhil030901 <jumbo.nikhil19@gmail.com>
2023-12-14 00:24:49 +0530

    fix index.js file

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
-import { sendFile } from "express/lib/response";
 
 dotenv.config();
 mongoose
